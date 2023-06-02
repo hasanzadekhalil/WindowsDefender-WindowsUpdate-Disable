03e1f3f4fffb0ece8131814718eddbc46bcdd087
Khalil Hasanzade <112690645+hasanzadekhalil@users.noreply.github.com>
2023-06-02 11:05:53 +0400

    Create DisableScript.ps1

---
+++ DisableScript.ps1
@@ -0,0 +1,17 @@
+$DefenderStatus = (Get-MpPreference).DisableRealtimeMonitoring
+
+if ($DefenderStatus -eq $false) {
+    Write-Host "Defender is Running! All Features of Windows Security need to be disabled manually the first time, please do this first and then run script again!"
+} else {
+    Write-Host "Defender is Not Running"
+
+    $downloadUrl = "https://github.com/qtkite/defender-control/releases/download/v1.5/disable-defender.exe"
+    $downloadPath = "$env:TEMP\disable-defender.exe"
+
+    # Download the application
+    Invoke-WebRequest -Uri $downloadUrl -OutFile $downloadPath
+
+    # Run the downloaded application as administrator
+    Start-Process -FilePath $downloadPath -Verb RunAs
+
+}
